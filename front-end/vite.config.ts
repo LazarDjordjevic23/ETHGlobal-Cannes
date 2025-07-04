0ee533f313a8bbaf8972b23508d91edf33b1cbf5
KobeJaimes <nikolaskadric.ns@gmail.com>
2025-07-04 14:35:04 +0200

    add FE inital

---
+++ front-end/vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@public": resolve(__dirname, "./public"),
+    },
+  },
+});
