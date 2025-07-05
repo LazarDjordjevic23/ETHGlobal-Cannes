80d60ea074a0a8ab0dd6be57fd3ac1e59184860f
LazarDjordjevic23 <djordjeviclazar12@gmail.com>
2025-07-05 19:12:04 +0200

    Deployed whole abstract wallet

--- contracts/scripts/VoteOnProposal.s.sol
+++ contracts/scripts/VoteOnProposal.s.sol
@@ -25,8 +25,8 @@ contract VoteOnProposal is BaseScript {
         // Get governance address
         address governanceAddress = getContractAddress(addressJson, network, "Governance");
 
-        uint256 proposalId = 95959446933743489345185149661070899877626769715576455202852791557849037682870;
-        uint256 support = 1;
+        uint256 proposalId = 81882712182884131001518211983589859132345454893186885172772635910663584985254;
+        uint8 support = 1;
         console.log("Voting on proposal", proposalId, "on network:", network);
         console.log("Governance:", governanceAddress);
 
