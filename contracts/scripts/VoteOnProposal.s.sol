3a6f3696082379e72b9ef3a37559b043721ff5ec
LazarDjordjevic23 <djordjeviclazar12@gmail.com>
2025-07-05 21:11:06 +0200

    fixed addresses

--- contracts/scripts/VoteOnProposal.s.sol
+++ contracts/scripts/VoteOnProposal.s.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.28;
 import "./BaseScript.s.sol";
 import {Governance} from "../src/Governance.sol";
 
-contract VoteOnProposal is BaseScript {
+contract VoteOnLastProposal is BaseScript {
 
     /**
-     * Run this command to vote on a proposal:
-     * forge script scripts/VoteOnProposal.s.sol --rpc-url sepolia --broadcast
+     * Run this command to vote on the last proposal:
+     * forge script scripts/VoteOnLastProposal.s.sol --rpc-url sepolia --broadcast
      *
      * 0 = Against
      * 1 = For
@@ -25,15 +25,32 @@ contract VoteOnProposal is BaseScript {
         // Get governance address
         address governanceAddress = getContractAddress(addressJson, network, "Governance");
 
-        uint256 proposalId = 81882712182884131001518211983589859132345454893186885172772635910663584985254;
-        uint8 support = 1;
-        console.log("Voting on proposal", proposalId, "on network:", network);
+        console.log("Getting last proposal on network:", network);
         console.log("Governance:", governanceAddress);
 
         // Initialize governance contract
         Governance governance = Governance(payable(governanceAddress));
-        governance.castVote(proposalId, support);
 
-        console.log("Vote cast successfully!");
+        // Get total proposals
+        uint256 proposalCount = governance.proposalCount();
+        console.log("Total proposals:", proposalCount);
+
+        require(proposalCount > 0, "No proposals found");
+
+        // Get last proposal ID
+        uint256 lastIndex = proposalCount - 1;
+        (uint256 lastProposalId,,,, ) = governance.proposalDetailsAt(lastIndex);
+
+        console.log("Last proposal ID:", lastProposalId);
+
+        // Vote settings
+        uint8 support = 1; // 1 = For, 0 = Against, 2 = Abstain
+
+        console.log("Casting vote (1=For, 0=Against, 2=Abstain):", support);
+
+        // Cast vote on the last proposal
+        governance.castVote(lastProposalId, support);
+
+        console.log("Vote cast successfully on proposal:", lastProposalId);
     }
 }
